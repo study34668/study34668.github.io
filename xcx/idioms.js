6aaf60f86d4f4dbc225d53d673a1c87ab4d2d3cc
张瑀晖 <2551730844@qq.com>
2019-04-15 22:04:55 +0800

    Site updated: 2019-04-15 22:04:53

---
+++ xcx/idioms.js
@@ -0,0 +1,59 @@
+loadIdioms = function() {
+    var idioms = [];
+
+    var idiom = [];
+    idiom[0] = Array('邯','郸','学','步');
+    idiom[1] = Array('生搬','硬套','机械','一味','模仿','学不到','原来的');
+    idiom[2] = '邯郸学步:形容生搬硬套,一味模仿,学不到新的,还忘了原来的';
+    idioms.push(idiom);
+	
+	idiom = [];
+	idiom[0] = Array('滥','竽','充','数');
+	idiom[1] = Array('没有','真才实学','混','凑数','名不副实','混水摸鱼');
+	idiom[2] = '滥竽充数:比喻没有真才实学的人,混在行家里面凑数';
+	idioms.push(idiom);
+	
+	idiom = [];
+	idiom[0] = Array('掩','耳','盗','铃');
+	idiom[1] = Array('以为','听不见','自欺欺人','自己欺骗自己');
+	idiom[2] = '掩耳盗铃:以为自己听不见别人也听不见,比喻自欺欺人';
+	idioms.push(idiom);
+	
+	idiom = [];
+	idiom[0] = Array('自','相','矛','盾');
+	idiom[1] = Array('不统一','不一致','前后抵触','不相应合');
+	idiom[2] = '自相矛盾:比喻人的语言行动前后抵触、不相应合';
+	idioms.push(idiom);
+	
+	idiom = [];
+	idiom[0] = Array('刻','舟','求','剑');
+	idiom[1] = Array('思维僵化','墨守陈规','拘泥','死守','固执','不变通','刻板');
+	idiom[2] = '刻舟求剑:比喻人思维僵化,墨守陈规,不与客观事物发展变化同步';
+	idioms.push(idiom);
+	
+	idiom = [];
+	idiom[0] = Array('画','蛇','添','足');
+	idiom[1] = Array('多此一举','多余','适得其反','弄巧成拙','得不偿失','无益','有害');
+	idiom[2] = '画蛇添足:比喻做事多此一举，反而得不偿失';
+	idioms.push(idiom);
+	
+	idiom = [];
+	idiom[0] = Array('杞','人','忧','天');
+	idiom[1] = Array('忧虑','担心','不切实际','没根据');
+	idiom[2] = '杞人忧天:意思是总是去忧虑那些不切实际的事物';
+	idioms.push(idiom);
+	
+	idiom = [];
+	idiom[0] = Array('井','底','之','蛙');
+	idiom[1] = Array('没见识','见识少','见识短浅','短浅');
+	idiom[2] = '井底之蛙:比喻见识短浅的人';
+	idioms.push(idiom);
+	
+	idiom = [];
+	idiom[0] = Array('杯','弓','蛇','影');
+	idiom[1] = Array('疑神疑鬼','自相惊扰','错觉','自己吓自己','疑虑');
+	idiom[2] = '杯弓蛇影:比喻疑神疑鬼，自相惊扰';
+	idioms.push(idiom);
+
+    return idioms;
+}
