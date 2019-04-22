b26153cc7ce19d7c33bdbca939c05ba5bbb43b5e
张瑀晖 <2551730844@qq.com>
2019-04-22 22:36:29 +0800

    Site updated: 2019-04-22 22:36:25

---
+++ xcx/poems.js
@@ -0,0 +1,92 @@
+loadPoems = function() {
+	var poems = [];
+	
+	var poem = [];
+	poem[0] = '惠崇春江晚景';
+	poem[1] = '宋 苏轼';
+	poem[2] = '竹外桃花三两枝';
+	poem[3] = '春江水暖鸭先知';
+	poem[4] = '蒌蒿满地芦芽短';
+	poem[5] = '正是河豚欲上时';
+	poems.push(poem);
+	
+	poem = [];
+	poem[0] = '三衢道中';
+	poem[1] = '宋 曾几';
+	poem[2] = '梅子黄时日日晴';
+	poem[3] = '小溪泛尽却山行';
+	poem[4] = '绿阴不减来时路';
+	poem[5] = '添得黄鹂四五声';
+	poems.push(poem);
+	
+	poem = [];
+	poem[0] = '忆江南';
+	poem[1] = '唐 白居易';
+	poem[2] = '江南好';
+	poem[3] = '风景旧曾谙';
+	poem[4] = '日出江花红胜火';
+	poem[5] = '春来江水绿如蓝';
+	poem[6] = '能不忆江南';
+	poems.push(poem);
+	
+	poem = [];
+	poem[0] = '元日';
+	poem[1] = '宋 王安石';
+	poem[2] = '爆竹声中一岁除';
+	poem[3] = '春风送暖入屠苏';
+	poem[4] = '千门万户曈曈日';
+	poem[5] = '总把新桃换旧符';
+	poems.push(poem);
+	
+	poem = [];
+	poem[0] = '清明';
+	poem[1] = '唐 杜牧';
+	poem[2] = '清明时节雨纷纷';
+	poem[3] = '路上行人欲断魂';
+	poem[4] = '借问酒家何处有';
+	poem[5] = '牧童遥指杏花村';
+	poems.push(poem);
+	
+	poem = [];
+	poem[0] = '九月九日忆山东兄弟';
+	poem[1] = '唐 王维';
+	poem[2] = '独在异乡为异客';
+	poem[3] = '每逢佳节倍思亲';
+	poem[4] = '遥知兄弟登高处';
+	poem[5] = '遍插茱萸少一人';
+	poems.push(poem);
+	
+	poem = [];
+	poem[0] = '滁州西涧';
+	poem[1] = '唐 韦应物';
+	poem[2] = '独怜幽草涧边生';
+	poem[3] = '上有黄鹂深树鸣';
+	poem[4] = '春潮带雨晚来急';
+	poem[5] = '野渡无人舟自横';
+	poems.push(poem);
+	
+	poem = [];
+	poem[0] = '大林寺桃花';
+	poem[1] = '唐 白居易';
+	poem[2] = '人间四月芳菲尽';
+	poem[3] = '山寺桃花始盛开';
+	poem[4] = '长恨春归无觅处';
+	poem[5] = '不知转入此中来';
+	poems.push(poem);
+	
+	poem = [];
+	poem[0] = '守株待兔';
+	poem[1] = '';
+	poem[2] = '宋人有耕田者';
+	poem[3] = '田中有株';
+	poem[4] = '兔走触株';
+	poem[5] = '折颈而死';
+	poem[6] = '因释其耒而守株';
+	poem[7] = '冀复得兔';
+	poem[8] = '兔不可复得';
+	poem[9] = '而身为宋国笑';
+	poems.push(poem);
+	
+	
+	return poems;
+}
