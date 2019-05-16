74564915a383982e8d6bf392bc3ce99c81d6e8c8
张瑀晖 <2551730844@qq.com>
2019-05-16 23:21:05 +0800

    Site updated: 2019-05-16 23:21:02

---
+++ xcx/means.js
@@ -0,0 +1,42 @@
+function getMeans() {
+	var means = [];
+	means.push('学校'); means.push('教室');
+	means.push('课本'); means.push('故事书');
+	means.push('语文书'); means.push('英语书');
+	means.push('练习本'); means.push('笔记本');
+	means.push('课桌'); means.push('椅子');
+	
+	means.push('一'); means.push('二');
+	means.push('三'); means.push('四');
+	means.push('五'); means.push('六');
+	means.push('七'); means.push('八');
+	means.push('九'); means.push('十');
+	means.push('十一'); means.push('十二');
+	
+	means.push('爸爸'); means.push('妈妈');
+	means.push('兄弟'); means.push('姐妹');
+	means.push('女儿'); means.push('儿子');
+	means.push('爷爷'); means.push('奶奶');
+	means.push('叔叔'); means.push('阿姨');
+	
+	means.push('芒果'); means.push('面包');
+	means.push('糖果'); means.push('面圈');
+	means.push('咖啡'); means.push('水果');
+	means.push('葡萄'); means.push('蜂蜜');
+	means.push('冰淇淋'); means.push('果酱');
+	
+	means.push('袋鼠'); means.push('狮子');
+	means.push('鸟巢'); means.push('猫头鹰');
+	means.push('鹦鹉'); means.push('鹌鹑');
+	means.push('公鸡'); means.push('大象');
+	means.push('老虎');
+	
+	means.push('裙子'); means.push('T恤衫');
+	means.push('制服'); means.push('背心');
+	means.push('表'); means.push('盒子');
+	means.push('游艇'); means.push('拉链');
+	means.push('斑马'); means.push('外套');
+	means.push('毛衣');
+	
+	return means;
+}
---
+++ xcx/words.js
@@ -0,0 +1,42 @@
+function getWords() {
+	var words = [];
+	words.push('school'); words.push('classroom');
+	words.push('textbook'); words.push('storybook');
+	words.push('Chinese book'); words.push('English book');
+	words.push('exercise book'); words.push('notebook');
+	words.push('desk'); words.push('chair');
+	
+	words.push('one'); words.push('two');
+	words.push('three'); words.push('four');
+	words.push('five'); words.push('six');
+	words.push('seven'); words.push('eight');
+	words.push('nine'); words.push('ten');
+	words.push('eleven'); words.push('twelve');
+	
+	words.push('father'); words.push('mother');
+	words.push('brother'); words.push('sister');
+	words.push('daughter'); words.push('son');
+	words.push('grandfather'); words.push('grandmother');
+	words.push('uncle'); words.push('aunt');
+	
+	words.push('mango'); words.push('bread');
+	words.push('candy'); words.push('doughnut');
+	words.push('coffee'); words.push('fruit');
+	words.push('grapes'); words.push('honey');
+	words.push('ice cream'); words.push('jam');
+	
+	words.push('kangaroo'); words.push('lion');
+	words.push('nest'); words.push('owl');
+	words.push('parrot'); words.push('quail');
+	words.push('rooster'); words.push('elephant');
+	words.push('tiger');
+	
+	words.push('skirt'); words.push('T-shirt');
+	words.push('uniform'); words.push('vest');
+	words.push('watch'); words.push('box');
+	words.push('yacht'); words.push('zip');
+	words.push('zebra'); words.push('coat');
+	words.push('sweater');
+	
+	return words;
+}
